ae0a6a29af470c4c3671aad754db1efc7d66de5f
Brad Thompson <gbradthompson@gmail.com>
2018-05-16 19:14:23 -0600

    remove /r page add error page

---
+++ layouts/error.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container pa-0>
+    <v-layout row wrap align-center justify-center>
+      <v-flex xs12 sm11 md11 lg10 xl8 py-4>
+        <h3 class="display-2 pa-4 text-xs-center">404 Page not found!</h3>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
